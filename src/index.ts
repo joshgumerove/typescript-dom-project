3e7d07debc479ec52abf7fbb0bb7c29e1f117854
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-15 13:47:15 -0500

    not null operator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn")!; // note the not null operator at the end
 // note this is before run-time (working with a value that may or may not exist)
 
-btn?.addEventListener("click", () => console.log("hello world"));
+btn.addEventListener("click", () => console.log("hello world"));
