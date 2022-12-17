02978499aa9dbe95e4f4570cdb6fa7b07a721122
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-16 21:47:03 -0500

    type casting with DOM library

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn"); // note the not null operator at the end
+const input = document.getElementById("todo-input"); // exclamation mark means it is certain to exist -- note how we cast the value here
 // note this is before run-time (working with a value that may or may not exist)
-btn.addEventListener("click", () => console.log("hello world"));
+btn.addEventListener("click", () => alert(input.value));
+// note the type assertion syntax
+// const mystery: unknown = "Hello world!!";
+// const numChars = (mystery as string).length; // does not change the type of mystery -- just in this specific context it is treated as a string
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-const btn = document.getElementById("btn")!; // note the not null operator at the end
+const btn: HTMLElement = document.getElementById("btn")!; // note the not null operator at the end
+
+const input = document.getElementById("todo-input")! as HTMLInputElement; // exclamation mark means it is certain to exist -- note how we cast the value here
 // note this is before run-time (working with a value that may or may not exist)
 
-btn.addEventListener("click", () => console.log("hello world"));
+btn.addEventListener("click", () => alert(input.value));
+
+// note the type assertion syntax
+
+// const mystery: unknown = "Hello world!!";
+
+// const numChars = (mystery as string).length; // does not change the type of mystery -- just in this specific context it is treated as a string
