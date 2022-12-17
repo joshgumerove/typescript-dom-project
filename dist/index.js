be73eaedeec8b6afa74d95a25afe7993a092b7e6
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-16 21:49:18 -0500

    reset input

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,10 @@
 const btn = document.getElementById("btn"); // note the not null operator at the end
 const input = document.getElementById("todo-input"); // exclamation mark means it is certain to exist -- note how we cast the value here
 // note this is before run-time (working with a value that may or may not exist)
-btn.addEventListener("click", () => alert(input.value));
+btn.addEventListener("click", () => {
+    alert(input.value);
+    input.value = "";
+});
 // note the type assertion syntax
 // const mystery: unknown = "Hello world!!";
 // const numChars = (mystery as string).length; // does not change the type of mystery -- just in this specific context it is treated as a string
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ const btn: HTMLElement = document.getElementById("btn")!; // note the not null o
 const input = document.getElementById("todo-input")! as HTMLInputElement; // exclamation mark means it is certain to exist -- note how we cast the value here
 // note this is before run-time (working with a value that may or may not exist)
 
-btn.addEventListener("click", () => alert(input.value));
+btn.addEventListener("click", () => {
+  alert(input.value);
+  input.value = "";
+});
 
 // note the type assertion syntax
 
 // const mystery: unknown = "Hello world!!";
 
 // const numChars = (mystery as string).length; // does not change the type of mystery -- just in this specific context it is treated as a string
+
+// need to remember to recompile when making changes
